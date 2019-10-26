a23ed1cc157a449c7097330ebdcea543ef8bec6a
Dan Dneprov <ddneprov_24@mail.ru>
2019-10-26 20:42:53 +0300

    third

---
+++ moscow_products/textCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  textCollectionViewCell.swift
+//  moscow_products
+//
+//  Created by Днепров Данила on 26/10/2019.
+//  Copyright © 2019 Днепров Данила. All rights reserved.
+//
+
+import UIKit
+
+class textCollectionViewCell: UICollectionViewCell {
+    
+}
